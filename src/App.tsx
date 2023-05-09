30b937efbc8bc844c471608861086469606fc5e9
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-10 01:59:27 +0530

    ex2 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,24 @@
 import { useState } from 'react'
 import './App.css'
-import NavBar from './components/NavBar';
-import Cart from './components/Cart';
+
 
 function App() {
-  const [game, setGame] = useState({
-    id: 1,
-    player:{
-      name : "Mohamed"
-    }
+  const [pizza, setPizza] = useState({
+    name : 'Spicy Pepperoni',
+    toppings : ['Mushroom']
   })
-
   const handleClick = () =>{
-    setGame({
-      ...game,
-      player : {
-        ...game.player,
-        name : "Kaif"
-      }
+    setPizza({
+      ...pizza,
+      toppings:[...pizza.toppings, 'Chicken']
     })
   }
-  // const [bugs, setBugs] = useState([
-  //   {id: 1, title: 'Bug1', fixed: false},
-  //   {id: 2, title: 'Bug2', fixed: false}
-  // ])
-
-  // const handleClick = () =>{
-  //   // setBugs(bugs.map(bug => bug.id === 1?{...bug, fixed: true}: {...bug}));
-  //   // console.log(bugs);
-  //   setBugs(produce(draft =>{
-  //     const bug = draft.find(bug => bug.id === 1);
-  //     if(bug) 
-  //       bug.fixed = true;
-  //   }))
-  // }
   return (
     <>
-      {/* <h1>Hey Kaif!</h1>
-      {bugs.map(bug => <p>{`${bug.id} is ${bug.fixed}`}</p>)}
-      <button onClick={handleClick}>Click Me</button> */}
-      {/* <NavBar CartItems={cartItems.length}/>
-      <Cart cartItems={cartItems} onClear={() =>{setCartItems([])}}/> */}
-      <h1>Hi {game.player.name}!</h1>
-      <button onClick={handleClick}>Change</button>
+    <ul>
+      {pizza.toppings.map(t => <li>{t}</li>)}
+    </ul>
+      <button onClick={handleClick}>Add!</button>
     </>
   )
 }
