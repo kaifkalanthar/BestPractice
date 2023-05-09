15543a77cb63bbc60c1ce646a5383bbf02d13939
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-10 02:17:11 +0530

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,24 @@ import './App.css'
 
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name : 'Spicy Pepperoni',
-    toppings : ['Mushroom']
+  const [cart, setCart] = useState({
+    discount : .1,
+    items: [
+      {id: 1, title: 'Product1', quantity: 1},
+      {id: 2, title: 'Product2', quantity: 1}
+    ]
   })
+
   const handleClick = () =>{
-    setPizza({
-      ...pizza,
-      toppings:[...pizza.toppings, 'Chicken']
+    setCart({
+      ...cart,
+      items: cart.items.map(item => item.id === 1?{...item, quantity: item.quantity+1}:item)
     })
   }
   return (
     <>
-    <ul>
-      {pizza.toppings.map(t => <li>{t}</li>)}
-    </ul>
-      <button onClick={handleClick}>Add!</button>
+    {cart.items.map(item => <p>{item.title} {item.quantity}</p>)}
+    <button onClick={handleClick}>Update!</button>
     </>
   )
 }
