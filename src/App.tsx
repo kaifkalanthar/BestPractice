ec03d998fd42ecb8647a2b379d005ca153cae2b7
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-05-10 17:53:41 +0530

    refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import './App.css'
-import ExpandableText from './components/ExpandableText'
+import Form from './components/Form'
+
 
 
 function App() {
 
   return (
     <>
-      <ExpandableText>Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-        irure dolor in reprehenderit in voluptate velit esse cillum dolore
-        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </ExpandableText>
+      <Form />
     </>
   )
 }
